1ccded017bf4e418a6a2b1ce784ce3ddb7d1103b
marcelo-olivera <marcelo.oliveira.256@gmail.com>
2025-01-10 19:38:55 -0300

    adicionar usuários

--- setup/urls.py
+++ setup/urls.py
@@ -1,5 +1,5 @@
 """
-URL configuration for detup project.
+URL configuration for setup project.
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/5.1/topics/http/urls/
@@ -22,4 +22,5 @@ from django.conf.urls.static import static
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('galeria.urls')),
+    path('', include('usuarios.urls')),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
